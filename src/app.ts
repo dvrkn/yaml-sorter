83335cb5a1f3ead4a4038c95b0accd02b56d6e35
Ilya Dvorkin <ilya.dvorkin@ownera.io>
2024-06-20 13:45:54 +0300

    linter and comment

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
+// NO LLM WAS HARMED IN THE MAKING OF THIS CODE
+//
+// This is a simple yaml sorter that sorts yaml files based on a predefined order of keys
+//
 import * as fs from 'fs';
 import * as yaml from 'js-yaml';
 
 const file = fs.readFileSync('test.yaml', 'utf8');
 const data = yaml.load(file);
+
+// The order of keys having precedence
+//
 const preOrder = [
     'enabled',
     'apiVersion',
@@ -28,8 +35,14 @@ const preOrder = [
     'destination',
     'sources',
 ];
+
+// The key to sort the array
+//
 const arraySortKey='name'
 
+// Array sort function
+// The sort based on the arraySortKey
+//
 function replacer(key, value) {
     if (Array.isArray(value)) {
         if (value.length > 0) {
@@ -48,15 +61,18 @@ function replacer(key, value) {
     return value;
 }
 
+// Yaml key sort function
+// The sort based on the preOrder array
+//
 function sortKeys(a, b) {
-    if (preOrder.indexOf(a) != -1 && preOrder.indexOf(b) != -1) {
+    if (preOrder.indexOf(a) !== -1 && preOrder.indexOf(b) !== -1) {
         if (preOrder.indexOf(a) > preOrder.indexOf(b)) return 1;
         if (preOrder.indexOf(a) < preOrder.indexOf(b)) return -1;
         return 0;
     }
 
-    if (preOrder.indexOf(a) != -1) return -1;
-    if (preOrder.indexOf(b) != -1) return 1;
+    if (preOrder.indexOf(a) !== -1) return -1;
+    if (preOrder.indexOf(b) !== -1) return 1;
 
     if (a < b) return -1;
     if (a > b) return 1;
@@ -64,6 +80,8 @@ function sortKeys(a, b) {
     return 0;
 }
 
+// Dump the yaml file
+//
 console.log(yaml.dump(data, {
     replacer: (key, value) => replacer(key, value),
     sortKeys: (a, b) => sortKeys(a, b)
