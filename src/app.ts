f2172fb7d111702d51a4900e44ca899f8cd39ad7
Ilya Dvorkin <ilya.dvorkin@ownera.io>
2024-06-20 12:37:50 +0300

    fix logic

--- src/app.ts
+++ src/app.ts
@@ -4,58 +4,59 @@ import * as yaml from 'js-yaml';
 
 const file = fs.readFileSync('test.yaml', 'utf8');
 const data = yaml.load(file);
+const preOrder = [
+    'enabled',
+    'apiVersion',
+    'kind',
+    'metadata',
+    'name',
+    'namespace',
+    'labels',
+    'annotations',
+    'goTemplate',
+    'generators',
+    'spec',
+    'containers',
+    'image',
+    'ports',
+    'protocol',
+    'resources',
+    'limits',
+    'requests',
+    'cpu',
+    'memory',
+    'volumeMount',
+    'destination',
+    'sources',
+];
+
 
 function sortKeys(a, b) {
-    const preOrder = [
-        'enabled',
-        'apiVersion',
-        'kind',
-        'metadata',
-        'name',
-        'namespace',
-        'labels',
-        'annotations',
-        'goTemplate',
-        'generators',
-        'spec',
-        'containers',
-        'image',
-        'ports',
-        'protocol',
-        'resources',
-        'limits',
-        'requests',
-        'cpu',
-        'memory',
-        'volumeMount',
-        'destination',
-        'sources',
-    ];
-
-    if (preOrder.indexOf(a) != -1) {
-        if (preOrder.indexOf(b) != -1) {
-            if (preOrder.indexOf(a) > preOrder.indexOf(b)) {
-                return 1;
-            }
-            if (preOrder.indexOf(a) < preOrder.indexOf(b)) {
-                return -1;
-            }
-        } else {
-            if (a > b) {
-                return 1;
-            }
-            if (a < b) {
-                return -1;
-            }
-            return 0;
-        }
-    } else {
-        if (preOrder.indexOf(b) != -1) {
+    if (preOrder.indexOf(a) != -1 && preOrder.indexOf(b) != -1) {
+        if (preOrder.indexOf(a) > preOrder.indexOf(b)) {
             return 1;
-        } else {
+        }
+        if (preOrder.indexOf(a) < preOrder.indexOf(b)) {
             return -1;
         }
+        return 0;
     }
+
+    if (preOrder.indexOf(a) != -1) {
+        return -1;
+    }
+    if (preOrder.indexOf(b) != -1) {
+        return 1;
+    }
+
+    if (a < b) {
+        return -1;
+    }
+
+    if (a > b) {
+        return 1;
+    }
+    return 0;
 }
 
 console.log(yaml.dump(data, {
